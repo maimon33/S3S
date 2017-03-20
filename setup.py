e4edddce8ca9bd8fa0729e289a68561830c5771f
Assi Maimon <maimon33@gmail.com>
2017-03-20 07:54:32 +0200

    start s3s

---
+++ setup.py
@@ -0,0 +1,36 @@
+########
+# Copyright (c) 2013 GigaSpaces Technologies Ltd. All rights reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+#    * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    * See the License for the specific language governing permissions and
+#    * limitations under the License.
+
+from setuptools import setup
+
+setup(
+    name='s3s',
+    version='0.1.1',
+    author='Assi Maimon',
+    author_email='maimon33@gmail.com',
+    packages=['s3s'],
+    license='LICENSE',
+    description='A Simple S3 upload tool',
+    entry_points={
+        'console_scripts': [
+            's3s = s3s.cli:_s3s'
+        ]
+    },
+    install_requires=[
+        'click==6.6',
+        'requests==2.7.0',
+        'click_didyoumean==0.0.3',
+    ]
+)
